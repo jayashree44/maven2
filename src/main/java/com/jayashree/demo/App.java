b23535d3922d5d855b766c0433f465cd07d14cd8
jayashree <jayashree.chowdary44@gmail.com>
2018-01-05 09:13:55 +0530

    lkj

---
+++ src/main/java/com/jayashree/demo/App.java
@@ -0,0 +1,13 @@
+package com.jayashree.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
